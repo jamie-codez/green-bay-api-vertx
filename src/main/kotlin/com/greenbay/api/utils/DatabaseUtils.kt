9b6f0ec76a47c53f6b577a5fdc179d96030d366d
jamie-codez <cruiseomondi90@gmail.com>
2023-06-05 16:49:10 +0300

    Working on DBUtils

---
+++ src/main/kotlin/com/greenbay/api/utils/DatabaseUtils.kt
@@ -0,0 +1,250 @@
+package com.greenbay.api.utils
+
+import io.vertx.core.Vertx
+import io.vertx.core.impl.logging.LoggerFactory
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.mongo.FindOptions
+import io.vertx.ext.mongo.MongoClient
+
+/**
+ * The Database Utilities class for DB operations
+ * @param vertx The vertx instance to be used
+ * @author Jamie Omondi
+ * @since 05-03-2023
+ * @see Vertx
+ */
+class DatabaseUtils(private val vertx: Vertx) {
+    /**
+     * For logging purposes
+     */
+    private val logger = LoggerFactory.getLogger(this.javaClass.simpleName)
+
+    /**
+     * The db client that will be used to perform all the CRUD operations in and out of the DB
+     */
+    private lateinit var mongoClient: MongoClient
+
+    init {
+        mongoClient = MongoClient.createShared(vertx, getDBConfig())
+    }
+
+    /**
+     * Provides the instance of mongo client created in this class
+     * @author Jamie Omondi
+     * @since 05-06-2023
+     * @see MongoClient
+     */
+
+    fun getMongoClient() = this.mongoClient
+
+    /**
+     * Provides the database configurations to the client
+     * @author Jamie Omondi
+     * @since 05-06-2023
+     */
+    private fun getDBConfig() = JsonObject.of(
+        "keepAlive", true,
+        "socketTimeoutMS", 5_000,
+        "connectTimeoutMS", 5_000,
+        "maxIdleTimeMS", 90_000,
+        "autoReconnect", true,
+        "db_name", System.getenv("GB_DB_NAME"),
+        "url", System.getenv("GB_DB_CON_STRING"),
+        "username", System.getenv("GB_DB_USERNAME"),
+        "password", System.getenv("GB_DB_PASSWORD"),
+        "authSource", "admin"
+    )
+
+    /**
+     * Inserts the parsed json document to the named collection
+     * @param collection The collection to house the data
+     * @param document the document to be saved
+     * @param success Th callback function called when operation is successful
+     * @param fail The callback function called when operation fails
+     * @author Jamie Omondi
+     * @since 05-06-2023
+     */
+    fun insert(
+        collection: String,
+        document: JsonObject,
+        success: (docId: String) -> Unit,
+        fail: (throwable: Throwable) -> Unit
+    ) {
+        logger.info("insert() -->")
+        this.getMongoClient().insert(collection, document) {
+            if (it.succeeded()) {
+                success(it.result())
+            } else {
+                logger.error("insert(${it.cause().message}) <--")
+                fail(it.cause())
+            }
+        }
+        logger.info("insert() <--")
+    }
+
+    /**
+     * Saves the parsed json document to the named collection -> Works same as insert
+     * @param collection The collection to house the data
+     * @param document the document to be saved
+     * @param success Th callback function called when operation is successful
+     * @param fail The callback function called when operation fails
+     * @author Jamie Omondi
+     * @since 05-06-2023
+     */
+    fun save(
+        collection: String,
+        document: JsonObject,
+        success: (docId: String) -> Unit,
+        fail: (throwable: Throwable) -> Unit
+    ) {
+        logger.info("insert() -->")
+        this.getMongoClient().save(collection, document) {
+            if (it.succeeded()) {
+                success(it.result())
+            } else {
+                logger.error("insert(${it.cause().message}) <--")
+                fail(it.cause())
+            }
+        }
+        logger.info("insert() <--")
+    }
+
+    /**
+     * Creates an index in the parsed collection on the fields parsed
+     * @param collection The collection housing the documents to be indexed
+     * @param fields The fields to be indexed
+     * @param success The callback function called when operation was successful
+     * @param fail The callback function called when operation failed
+     * @author Jamie Omondi
+     * @since 05-06-2023
+     */
+    fun createIndex(
+        collection: String,
+        fields: JsonObject,
+        success: (void: Void) -> Unit,
+        fail: (throwable: Throwable) -> Unit
+    ) {
+        logger.info("createIndex() -->")
+        this.getMongoClient().createIndex(collection, fields) {
+            if (it.succeeded()) {
+                success(it.result())
+            } else {
+                logger.error("createIndex(${it.cause().message}) <--")
+                fail(it.cause())
+            }
+        }
+        logger.info("createIndex() <--")
+    }
+
+    /**
+     * Drops an index in the parsed collection
+     * @param collection The collection housing the documents with the indexed
+     * @param indexName The name of the index
+     * @param success The callback function called when operation was successful
+     * @param fail The callback function called when operation failed
+     * @author Jamie Omondi
+     * @since 05-06-2023
+     */
+    fun createIndex(
+        collection: String,
+        indexName: String,
+        success: (void: Void) -> Unit,
+        fail: (throwable: Throwable) -> Unit
+    ) {
+        logger.info("dropIndex() -->")
+        this.getMongoClient().dropIndex(collection, indexName) {
+            if (it.succeeded()) {
+                success(it.result())
+            } else {
+                logger.error("dropIndex(${it.cause().message}) <--")
+                fail(it.cause())
+            }
+        }
+        logger.info("dropIndex() <--")
+    }
+
+    /**
+     * Gets one document from the collection that is distinct with the given query
+     * @param collection The collection housing the document
+     * @param query The query used to filter the data
+     * @param fields The fields that you want to be returned in the document
+     * @param success The callback function called when operation is successful
+     * @param fail The callback function called when operation fails
+     */
+    fun findOne(
+        collection: String,
+        query: JsonObject,
+        fields: JsonObject,
+        success: (result: JsonObject) -> Unit,
+        fail: (throwable: Throwable) -> Unit
+    ) {
+        logger.info("findOne() -->")
+        this.getMongoClient().findOne(collection, query, fields) {
+            if (it.succeeded()) {
+                success(it.result())
+            } else {
+                logger.error("findOne(${it.cause().message}) <--")
+                fail(it.cause())
+            }
+        }
+        logger.info("findOne() <--")
+    }
+
+    /**
+     * Fetches documents form the given collection filtering wit the parsed query
+     * @param collection The collection housing the documents
+     * @param query The query used to filter the data
+     * @param success The callback function called when operation is successful
+     * @param fail The callback function called when operation fails
+     * @author Jamie Omondi
+     * @since 05-06-2023
+     */
+    fun find(
+        collection: String,
+        query: JsonObject,
+        success: (result: List<JsonObject>) -> Unit,
+        fail: (throwable: Throwable) -> Unit
+    ) {
+        logger.info("find() -->")
+        this.getMongoClient().find(collection, query) {
+            if (it.succeeded()) {
+                success(it.result())
+            } else {
+                logger.error("find(${it.cause().message}) <--")
+                fail(it.cause())
+            }
+        }
+        logger.info("find() <--")
+    }
+
+    /**
+     * Gets documents from a collection with the given query and find options parsed
+     * @param collection The collection housing the data
+     * @param query The query use to filter the data
+     * @param findOptions The findOptions to be used to query dat from the collection
+     * @param success The callback function called when the operation is successful
+     * @param fail The callback function called when the operation failed
+     * @author Jamie Omondi
+     * @since 05-06-2-23
+     */
+    fun findWithOptions(
+        collection: String,
+        query: JsonObject,
+        findOptions: FindOptions,
+        success: (result: List<JsonObject>) -> Unit,
+        fail: (throwable: Throwable) -> Unit
+    ) {
+        logger.info("findWithOptions() -->")
+        this.getMongoClient().findWithOptions(collection, query, findOptions) {
+            if (it.succeeded()) {
+                success(it.result())
+            } else {
+                logger.error("findWithOptions(${it.cause().message}) <--")
+                fail(it.cause())
+            }
+        }
+        logger.info("findWithOptions() <--")
+    }
+
+
+}
