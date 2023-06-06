dd184a999c682e828f5cee87b28edd573fb06935
jamie-codez <cruiseomondi90@gmail.com>
2023-06-07 00:17:40 +0300

    Achieved clean architecture successfully

--- src/main/kotlin/com/greenbay/api/services/user/UserService.kt
+++ src/main/kotlin/com/greenbay/api/services/user/UserService.kt
@@ -1,6 +1,9 @@
 package com.greenbay.api.services.user
 
 import com.greenbay.api.utils.BaseUtils.Companion.execute
+import com.greenbay.api.utils.BaseUtils.Companion.getResponse
+import io.netty.handler.codec.http.HttpResponseStatus.CREATED
+import io.netty.handler.codec.http.HttpResponseStatus.INTERNAL_SERVER_ERROR
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
@@ -17,8 +20,14 @@ class UserService : AbstractVerticle() {
         execute("createUser", rc, { body, params, response ->
             try {
                 val user = body.mapTo(AppUser::class.java)
-            }catch (ex:Exception){
+                service.createUser(user, {
+                    response.end(getResponse(CREATED.code(),"User created successfully"))
+                }, {
+                    response.end(getResponse(INTERNAL_SERVER_ERROR.code(), "Error occurred try again"))
+                })
+            } catch (ex: Exception) {
                 println(ex.message)
+                response.end(getResponse(INTERNAL_SERVER_ERROR.code(), "${ex.message}"))
             }
         }, "username", "firstName", "lastName", "email", "phone", "")
     }
