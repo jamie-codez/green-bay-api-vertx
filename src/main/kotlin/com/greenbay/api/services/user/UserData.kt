c833a436ae3273610a1447b26d2679dd6df5dc98
jamie-codez <cruiseomondi90@gmail.com>
2023-06-07 00:14:17 +0300

    Testing waters

--- src/main/kotlin/com/greenbay/api/services/user/UserData.kt
+++ src/main/kotlin/com/greenbay/api/services/user/UserData.kt
@@ -2,6 +2,7 @@ package com.greenbay.api.services.user
 
 data class AppUser(
     val uid:String,
+    val username:String,
     val firstName:String,
     val lastName:String,
     val email:String,
--- src/main/kotlin/com/greenbay/api/services/user/UserService.kt
+++ src/main/kotlin/com/greenbay/api/services/user/UserService.kt
@@ -1,8 +1,25 @@
 package com.greenbay.api.services.user
 
+import com.greenbay.api.utils.BaseUtils.Companion.execute
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
 
-class UserService:AbstractVerticle() {
+class UserService : AbstractVerticle() {
     val service = ServiceEngine(this.getVertx())
 
+    fun setUserRoutes(router: Router) {
+        router.post("/user").handler(::createUser)
+    }
+
+    private fun createUser(rc: RoutingContext) {
+        execute("createUser", rc, { body, params, response ->
+            try {
+                val user = body.mapTo(AppUser::class.java)
+            }catch (ex:Exception){
+                println(ex.message)
+            }
+        }, "username", "firstName", "lastName", "email", "phone", "")
+    }
 }
